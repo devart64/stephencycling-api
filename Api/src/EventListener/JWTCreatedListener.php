23e3f58d63e191615e68a5cda7e2369b2623a57c
Stephen Dupré <stephendupre64@gmail.com>
2020-10-06 08:16:04 +0200

    add swagger version fo fixed bug

---
+++ Api/src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\EventListener;
+
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class JWTCreatedListener
+{
+    /**
+     * @var UserInterface
+     */
+    private $user;
+
+    public function __construct(Security $security) {
+    $this->user = $security->getUser();
+    }
+
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $payload = $event->getData();
+        $payload['createdAt'] = $this->user->getCreatedAt();
+        $event->setData($payload);
+    }
+}
